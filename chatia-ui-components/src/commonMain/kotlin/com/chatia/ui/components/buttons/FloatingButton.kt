fd2f719dddc0ca1d9ed9a0139f4385ec20b1e5a0
mohamedashraf <mohamed.ashraf.rady0@gmail.com>
2025-09-06 15:36:26 +0300

    ci

--- chatia-ui-components/src/commonMain/kotlin/com/chatia/ui/components/buttons/FloatingButton.kt
+++ chatia-ui-components/src/commonMain/kotlin/com/chatia/ui/components/buttons/FloatingButton.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.painterResource
 
+
 @Composable
 fun FloatingActionButton(
     modifier: Modifier = Modifier,
